b3dfb6bbbee9c7592d56caaffc86a02940e1388b
Jon Mockbee <mockbee1000@gmail.com>
2021-08-10 16:53:21 -0400

    I thinki this might be right

--- question6.py
+++ question6.py
@@ -1,6 +1,12 @@
 def format_name(first_name, last_name):
-	# code goes here
-	
+	if first_name=='' and last_name != '':
+        return "Name: "+last_name
+    elif last_name =='' and first_name !='':
+        return "Name: "+first_name
+    elif first_name=='' and last_name == '':
+        return ''
+    else:
+        return 'Name: '+ last_name+', '+ first_name    
 	return string 
 
 print(format_name("Ernest", "Hemingway"))
